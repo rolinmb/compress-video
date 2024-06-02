c76da17fd6aee42f1e7420f4891bfcaa71b48197
Rolin Blake <rolinmblake@gmail.com>
2024-06-02 03:22:35 -0500

    init

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "os/exec"
+)
+
+const (
+  VIDEOIN = "video-in/teaser0_06022024.mp4"
+  OUTNAME = "video-out/teser0_comp.mp4"
+)
+
+func main() {
+  
+}
