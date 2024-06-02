c779031f701b348c3aef00965329493a4e92845a
Rolin Blake <rolinmblake@gmail.com>
2024-06-02 03:46:36 -0500

    troubleshooting

--- src/main.go
+++ src/main.go
@@ -9,8 +9,25 @@ import (
 const (
   VIDEOIN = "video-in/teaser0_06022024.mp4"
   OUTNAME = "video-out/teser0_comp.mp4"
+  //DURATION = 34
+  //TARGETMB = 20
 )
 
 func main() {
-  
+  //targetBitrate := (TARGETMB * 1024 * 1024 * 8) / DURATION
+  ffmpegCmd := exec.Command(
+    "ffmpeg",
+    "-i", VIDEOIN,
+    "-b:v", "4934k",
+    "-c:a", "aac",
+    "-b:a", "190k",
+    "-ac", "2",
+    "-t", "34",
+    OUTNAME,
+  )
+  ffmpegOutput, err := ffmpegCmd.CombinedOutput()
+  if err != nil {
+    log.Fatalf("\nmain(): An error occured while running ffmpegCmd: %s\n%v", string(ffmpegOutput), err)
+  }
+  fmt.Printf("\nmain(): Successfully compressed video %s and saved as %s with ffmpeg\n\n%s", VIDEOIN, OUTNAME, string(ffmpegOutput))
 }
